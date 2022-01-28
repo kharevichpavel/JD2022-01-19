4cb92119af996ece362748e15592e5ca90349523
Andrey Katsal <katsal95@mail.ru>
2022-01-28 17:53:58 +0300

    jd2022-01-19 primer

--- src/by/it/katsal/jd01_05/TaskB.java
+++ src/by/it/katsal/jd01_05/TaskB.java
@@ -1,5 +1,23 @@
 package by.it.katsal.jd01_05;
-
+import static java.lang.Math.*;
 public class TaskB {
+    public static void main(String[] args) {
+        step1();
+
+    }
+
+    private static void step1() {
+        for (double  a = 0; a <= 2; a = a + 0.2) {
+            System.out.printf("index=%2.1f%n",a);
+            for (int x = 1; x <=6 ; x++) {
+                double y = pow(7,a) -cos(x);
+
+
+                System.out.printf("index=%2.1f  final=%4.2f%n",a,y);
+            }
+
+
+        }
+    }
 
 }
