af32dda7e4c27b8da96f6ea03647fd10e4a1ddca
kharevichpavel <pashacrut84@gmail.com>
2022-01-29 14:58:47 +0300

    All Task

--- src/by/it/kharevich/jd01_04/TaskB.java
+++ src/by/it/kharevich/jd01_04/TaskB.java
@@ -23,9 +23,9 @@ public class TaskB {
             System.out.printf("Введите зарплату для %s\n", nameAll[i]);
             for (int j = 0; j < salary[0].length; j++) {
                 salary[i][j] = sc2.nextInt();
-                sum = sum + salary[i][j];
-                sumName = sumName + salary[0][j];
-                average = sum / salary.length;
+                sum=sum+salary[i][j];
+                sumName=sumName + salary[0][j];
+                average = sum/salary.length;
             }
         }
 
@@ -37,20 +37,17 @@ public class TaskB {
 
         for (int i = 0; i < salary.length; i++) {
             System.out.println();
-            System.out.printf("%-15s", nameAll[i] + ":");
+            System.out.printf("%-15s", nameAll[i]+":");
             for (int i1 = 0; i1 < salary[i].length; i1++) {
-                System.out.printf("%-10d", salary[i][i1]);
+                System.out.printf("%-10d",salary[i][i1]);
             }
             System.out.printf("%-10d", sumName);
         }
         System.out.println();
         System.out.print("-".repeat(65));
         System.out.println();
-        System.out.printf("Итого          " + "%-15d", sum);
+        System.out.printf("Итого          "+"%-15d",sum);
         System.out.println();
-        System.out.printf("Средняя        " + "%-15.5f", average);
+        System.out.printf("Средняя        "+"%-15.5f",average);
     }
 }
-
-
-
