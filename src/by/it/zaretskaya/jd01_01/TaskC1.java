45aa491b62a456271e1437b4671a35760f0e571c
galinaedyardovna <galinaedyardovnazaretskaya@gmail.com>
2022-01-30 17:27:21 +0300

    Task 1...4

---
+++ src/by/it/zaretskaya/jd01_01/TaskC1.java
@@ -0,0 +1,21 @@
+package by.it.zaretskaya.jd01_01;
+
+/* Нужно написать программу, которая вводит два числа с клавиатуры
+и выводит их сумму на экран в виде
+
+Ввод (это вы вводите с клавиатуры):
+34 26
+
+Вывод (это должно появиться в консоли, обратите внимание на пробелы и слово Sum:
+Sum = 60
+
+*/
+class TaskC1 {
+    public static void main(String[] args) {
+        int a=7;
+        int b=3;
+        int Result=a+b;
+        System.out.println( "Sum" + " = " +Result);
+
+    }
+}
