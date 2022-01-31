69268d7ec8c5e42df601e1736cf4252c4a39e3f9
Alexander Khmelov <a.khmelev@gmail.com>
2022-01-31 11:27:29 +0300

    Task1..4 Level B

--- src/by/it/khmelov/jd01_03/Helper.java
+++ src/by/it/khmelov/jd01_03/Helper.java
@@ -3,7 +3,30 @@ package by.it.khmelov.jd01_03;
 import java.util.Arrays;
 
 public class Helper {
+
+    public static void main(String[] args) {
+        double[] array = {10, 2, 5, 8, 9, 6, 4, 3, 2, 0};
+        sort(array);
+        System.out.println(Arrays.toString(array));
+    }
+
     public static void sort(double[] array) {
-        Arrays.sort(array);
+        // 0  1  2  3  4  5  6  7  8  9
+        //10, 2, 5, 8, 9, 6, 4, 3, 2, 0
+        //2  5   8  9  6  4  3  2  0 10
+        int lastIndex = array.length - 1;
+        boolean swap;
+        do {
+            swap = false;
+            for (int i = 0; i < lastIndex; i++) {
+                if (array[i] > array[i + 1]) {
+                    double temp = array[i];
+                    array[i] = array[i + 1];
+                    array[i + 1] = temp;
+                    swap = true;
+                }
+            }
+            lastIndex--;
+        } while (swap);
     }
 }
