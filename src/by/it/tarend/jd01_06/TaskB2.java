32ea05df2a704a0de2b224719be8792edf3c92c5
MaximTarend <dedmasim@gmail.com>
2022-02-01 22:13:36 +0300

    Task1..6 Tarend

--- src/by/it/tarend/jd01_06/TaskB2.java
+++ src/by/it/tarend/jd01_06/TaskB2.java
@@ -1,9 +1,12 @@
 package by.it.tarend.jd01_06;
 
+import java.util.Arrays;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class TaskB2 {
+
+    private static String[] sentences = {};
 
     public static void main(String[] args) {
         String regex = "[а-яА-ЯёЁ:,\\s-]+(\\.\\.\\.)?[а-яА-ЯёЁ:,\\s-]+";
@@ -11,20 +14,40 @@ public class TaskB2 {
         Matcher matcher = pattern.matcher(Poem.text);
         while (matcher.find()) {
             String sentence = matcher.group();
-            sentence = processSentence(sentence);
-
-            System.out.println(sentence);
+            processSentence(sentence);
         }
-
+        sortSentences(sentences);
+        System.out.println();
+        for (int i = 0; i < sentences.length; i++) {
+            System.out.println(sentences[i]);
+        }
 
     }
 
-    private static String processSentence(String sentence) {
+    private static void processSentence(String sentence) {
 
         String regex = "[^а-яА-яёЁ]+";
-        Pattern pattern = Pattern.compile(regex);
+        Pattern.compile(regex);
         sentence = sentence.replaceAll(regex, " ");
+        sentence = sentence.trim();
 
-        return sentence.trim();
+        sentences = Arrays.copyOf(sentences, sentences.length+1);
+        sentences[sentences.length - 1] = sentence;
+
+    }
+
+    private static void sortSentences(String[] sentences) {
+
+        for (int i = 0; i < sentences.length; i++) {
+            int minLengthIndex = i;
+            for (int j = i; j < sentences.length; j++) {
+                if (sentences[minLengthIndex].length() > sentences[j].length()) {
+                    minLengthIndex = j;
+                }
+            }
+            String buffer = sentences[i];
+            sentences[i] = sentences[minLengthIndex];
+            sentences[minLengthIndex] = buffer;
+        }
     }
 }
