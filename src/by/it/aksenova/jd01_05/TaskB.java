a0ca810a1d5f7179e01b540a75778538e0296512
Svetlana-01-2022 <svetlana.tkd@mail.ru>
2022-02-01 10:23:43 +0300

    jd01_05 B1 complite

--- src/by/it/aksenova/jd01_05/TaskB.java
+++ src/by/it/aksenova/jd01_05/TaskB.java
@@ -5,16 +5,15 @@ import static java.lang.Math.*;
 public class TaskB {
     public static void main(String[] args) {
         step1();
+        System.out.println("-".repeat(40));
         step2();
     }
 
         private static void step1 () {
-            int x;
-            for (x = 1; x < 7; x++) {
+            for (double a = 0; a<=2; a=a+0.2) {
                 double sum = 0;
-                for (double a = 0; a <= 2; a = a + 0.2) {
-                    sum = pow(7, a) - tan(x);
-                    double y = sum;
+                for (int x=1; x<7; x++) {
+                    sum = sum+ (pow(7, a) - cos(x));
                     System.out.printf("a=%3.2f sum=%g", a, sum);
                     System.out.println();
                 }
