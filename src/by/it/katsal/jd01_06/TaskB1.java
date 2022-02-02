b61c968fad5c4459ba865f95f9f2b4e0f6f552bb
Andrey Katsal <katsal95@mail.ru>
2022-02-02 17:27:50 +0300

    jd2022-01-19 primer

--- src/by/it/katsal/jd01_06/TaskB1.java
+++ src/by/it/katsal/jd01_06/TaskB1.java
@@ -1,20 +1,33 @@
 package by.it.katsal.jd01_06;
 
+
+import java.util.regex.Pattern;
+import java.util.regex.Matcher;
 public class TaskB1 {
 
-    private static  String[] letters = {"А а О о У у Э э Ы ы Я я Ё ё Е е Ю ю И и"};
+
 
     public static void main(String[] args) {
-        String word = Poem.text;
-
-
-
-    }
-    private static void processOneWord(String word) {
+        Pattern pattern = Pattern.compile("[а-яА-яёЁ]+");
+        Matcher matcher = pattern.matcher(Poem.text);
+             while(matcher.find()){
+              String word = matcher.group();
+                 if (processOneWord(word)){
+                   System.out.println(word );
+               }
+             }
+           }
+    private static boolean  processOneWord(String word) {
         char first = word.charAt(0);
         char last = word.charAt(word.length() - 1);
 
 
+
+
+
+        String[] letters = {"А а О о У у Э э Ы ы Я я Ё ё Е е Ю ю И и"};
+            return();
          }
+
     }
 
