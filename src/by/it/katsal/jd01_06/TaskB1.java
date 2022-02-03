57d0c1d68757f4853e3776319df07a6c4f46d493
Andrey Katsal <katsal95@mail.ru>
2022-02-03 18:37:22 +0300

    jd2022-01-19 primer

--- src/by/it/katsal/jd01_06/TaskB1.java
+++ src/by/it/katsal/jd01_06/TaskB1.java
@@ -1,6 +1,7 @@
 package by.it.katsal.jd01_06;
 
 
+import java.util.Locale;
 import java.util.regex.Pattern;
 import java.util.regex.Matcher;
 public class TaskB1 {
@@ -18,16 +19,14 @@ public class TaskB1 {
              }
            }
     private static boolean  processOneWord(String word) {
-        char first = word.charAt(0);
-        char last = word.charAt(word.length() - 1);
-
-
-
-
-
-        String[] letters = {"А а О о У у Э э Ы ы Я я Ё ё Е е Ю ю И и"};
-            return();
-         }
+        char firstLetters = word.charAt(0);
+        char lastLetters = word.charAt(word.length() - 1);
+        String vowels = "А а О о У у Э э Ы ы Я я Ё ё Е е Ю ю И и";
+        String consonants = "Б б В в Г г Д д Ж ж З з Й й К к Л л М м Н н П п Р р С с Т т Ф ф Х х Ц ц Ч ч Ш ш Щ щ";
+        consonants = consonants + consonants.toLowerCase(Locale.ROOT);
+        vowels = vowels.toLowerCase(Locale.ROOT);
+        return ((consonants.indexOf(firstLetters) >= 0 ) && (vowels.indexOf(lastLetters )>=0 ));
+    }
 
     }
 
