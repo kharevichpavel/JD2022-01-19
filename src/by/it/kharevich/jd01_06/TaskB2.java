1687cc1eccfe09205d1dd545a36b208bf5f3989f
kharevichpavel <pashacrut84@gmail.com>
2022-02-03 18:21:00 +0300

    jd01_07 TaskA1,2,3 TaskB1,2

--- src/by/it/kharevich/jd01_06/TaskB2.java
+++ src/by/it/kharevich/jd01_06/TaskB2.java
@@ -1,4 +1,24 @@
 package by.it.kharevich.jd01_06;
 
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class TaskB2 {
+
+    private static String[] words = {};
+
+    public static void main(String[] args) {
+        Pattern pattern = Pattern.compile("[а-яА-ЯёЁ]+");
+        Matcher matcher = pattern.matcher(by.it.kharevich.jd01_06.Poem.text);
+        while (matcher.find()) {
+            String word = matcher.group();
+
+
+        }
+    }
+
+
 }
+
+
---
+++ src/by/it/kharevich/jd01_07/Runner.java
@@ -0,0 +1,19 @@
+package by.it.kharevich.jd01_07;
+
+public class Runner {
+    public static void main(String[] args) {
+
+        Scalar var1=new Scalar("1234.5678");
+        System.out.println(var1);
+        Var var2=new Scalar(2.22);
+        System.out.println(var2);
+        Var var3=new Scalar(var1);
+        System.out.println(var3);
+
+        Vector var4=new Vector(new double[]{1.0,2.0,4.0});
+        System.out.println(var4);
+        Var var5=new Vector(var4);
+        System.out.println(var5);
+
+    }
+}
