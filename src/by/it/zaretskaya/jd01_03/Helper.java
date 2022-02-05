24e7a5294976205ebeb67893d7f5ad2aa73d913e
galinaedyardovna <galinaedyardovnazaretskaya@gmail.com>
2022-02-05 21:32:48 +0300

    Task 1...8 task3B

--- src/by/it/zaretskaya/jd01_03/Helper.java
+++ src/by/it/zaretskaya/jd01_03/Helper.java
@@ -1,48 +1,58 @@
 package by.it.zaretskaya.jd01_03;
 
+import com.sun.jdi.Value;
+
 public class Helper {
-     public static void main(String[] args) {
-         double[] array = {11, 22, 33, 66, 99, 0, 123, -9};
-         sort(array);
+    public static void main(String[] args) {
+        double[] array = {11, 22, 33, 66, 99, 0, 123, -9};
+        sort(array);
 
-     }
+    }
 
-     static void sort(double[] array) {
-         int lastIndex = array.length - 1;
-         boolean sortNotComplete;
-         do {
-             sortNotComplete=false;
+    static void sort(double[] array) {
+        int lastIndex = array.length - 1;
+        boolean sortNotComplete;
+        do {
+            sortNotComplete = false;
 
-             for (int i = 0; i < lastIndex; i++) {
-                 if (array[i] > array[i + 1]) {
-                     double temp = array[i];
-                     array[i] = array[i + 1];
-                     array[i + 1] = temp;
-                     sortNotComplete=true;
+            for (int i = 0; i < lastIndex; i++) {
+                if (array[i] > array[i + 1]) {
+                    double temp = array[i];
+                    array[i] = array[i + 1];
+                    array[i + 1] = temp;
+                    sortNotComplete = true;
 
-                 }
+                }
 
-             }
-             lastIndex--;
-         } while (sortNotComplete);
+            }
+            lastIndex--;
+        } while (sortNotComplete);
 
 
-     }
-     static double findMin (double[] array){
-         double min=0;
-         for (int i = 0; i < array.length; i++) {
-           if (array[i]<min); {
-               min=array[i];
-             }
-         }
-         return (min);
-     }
-     static double findMax(double[]array) {
-         double max = 0;
-         for (int i = 0; i < array.length; i++) {
-             if (array[i] > max) ;
-             max = array[i];
-         }
-         return (max);
-     }
- }
+    }
+
+    static double findMin(double[] array) {
+        if (0 == array.length) {
+            return Integer.MIN_VALUE;
+        } else {
+            double min = array[0];
+            for (double i : array) {
+                if (min > i) min = i;
+            }
+            return (min);
+        }
+    }
+
+    static double findMax(double[] array) {
+        if (0 == array.length) {
+            return Integer.MAX_VALUE;
+        } else {
+            double max = array[0];
+            for (double i : array) {
+                if (max < i) max = i;
+            }
+            return (max);
+
+        }
+    }
+}
