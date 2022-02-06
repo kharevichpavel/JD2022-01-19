6a9272ffbb5bfa4f1303aa543e30918ae79df32f
galinaedyardovna <galinaedyardovnazaretskaya@gmail.com>
2022-02-06 12:44:05 +0300

    Task 1...8 task3B,2C

--- src/by/it/zaretskaya/jd01_08/Vector.java
+++ src/by/it/zaretskaya/jd01_08/Vector.java
@@ -7,7 +7,19 @@ public Vector(double[] value) {
  this.value = value.clone();
 }
 
-@Override
+ @Override
+ public Var add(Var other) {
+ if(other instanceof Scalar scalar){
+  for (int i = 0; i < value.length; i++) {
+   value[i]+=scalar.getValue();
+  }
+  return new Vector (value);
+ }
+
+  return super.add(other);
+ }
+
+ @Override
 public String toString () {
  StringBuilder out = new StringBuilder();
  out.append("{");
