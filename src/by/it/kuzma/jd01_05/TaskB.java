232896d10a436a3b73677b6fdcc11a332ac5d630
KuzmaEV <yaugen93@yandex.ru>
2022-02-09 08:03:22 +0300

    Task_4_B2

--- src/by/it/kuzma/jd01_05/TaskB.java
+++ src/by/it/kuzma/jd01_05/TaskB.java
@@ -5,6 +5,8 @@ import static java.lang.Math.*;
 public class TaskB {
     public static void main(String[] args) {
         step1();
+        step2();
+
 
     }
 
@@ -22,5 +24,35 @@ public class TaskB {
 
 
         }
+    }
+
+    private static void step2() {
+
+        System.out.println("_".repeat(40));
+        System.out.println(" x      a");
+
+        for (double x = -6; x < 2; x+=0.5) {
+
+            double a;
+            double beta;
+
+            if (-2 < x/2 && x/2 <= -1){
+                beta = sin(pow(x, 2));
+                a = log10(abs(beta + 2.74));
+                System.out.printf("%4.2f  %f%n",x ,a);
+            }else if (-1 <x/2 && x/2 <0.2){
+                beta = cos(pow(x, 2));
+                a = log10(abs(beta + 2.74));
+                System.out.printf("%4.2f  %f%n",x ,a);
+            }else if (x/2 == 0.2){
+                beta = 1 / tan(pow(x, 2));
+                a = log10(abs(beta + 2.74));
+                System.out.printf("%4.2f  %f%n",x ,a);
+            }else {
+                System.out.printf("%4.2f  Вычисления невозможны%n", x);
+            }
+            
+        }
+
     }
 }
