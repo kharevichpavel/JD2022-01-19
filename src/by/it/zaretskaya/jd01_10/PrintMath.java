cce925eb06cc16953866c557f5fe08fb6fa621f0
galinaedyardovna <galinaedyardovnazaretskaya@gmail.com>
2022-02-10 11:31:33 +0300

    Task 1...10

--- src/by/it/zaretskaya/jd01_10/PrintMath.java
+++ src/by/it/zaretskaya/jd01_10/PrintMath.java
@@ -1,5 +1,6 @@
 package by.it.zaretskaya.jd01_10;
 
+import java.lang.reflect.Field;
 import java.lang.reflect.Method;
 import java.lang.reflect.Modifier;
 
@@ -36,6 +37,19 @@ public class PrintMath {
 
 
         }
+        Field[] declaredFields = structure.getDeclaredFields();
+        for (Field field : declaredFields) {
+            StringBuilder builder = new StringBuilder();
+            int fieldModifiers = field.getModifiers();
+            if (Modifier.isPublic(fieldModifiers)) {
+                
+
+            }
+
+
+
+        }
+
 
 
     }
