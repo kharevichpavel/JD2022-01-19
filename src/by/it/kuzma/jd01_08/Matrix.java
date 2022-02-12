fefbe7641eca4c95ec5fa541081a556ab11b5957
KuzmaEV <yaugen93@yandex.ru>
2022-02-12 20:46:23 +0300

    Task_8_B

---
+++ src/by/it/kuzma/jd01_08/Matrix.java
@@ -0,0 +1,17 @@
+package by.it.kuzma.jd01_08;
+
+import java.util.Arrays;
+
+class Matrix extends Var {
+
+    private double[][] value;
+
+    public Matrix(String valueStr) {
+
+    }
+
+    @Override
+    public String toString() {
+        return "Matrix{]";
+    }
+}
