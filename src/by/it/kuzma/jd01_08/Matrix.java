9375e271259bc946899274d70d2634f3f505f63c
KuzmaEV <yaugen93@yandex.ru>
2022-02-13 13:22:03 +0300

    Task_8_C

--- src/by/it/kuzma/jd01_08/Matrix.java
+++ src/by/it/kuzma/jd01_08/Matrix.java
@@ -1,17 +1,170 @@
 package by.it.kuzma.jd01_08;
 
-import java.util.Arrays;
-
 class Matrix extends Var {
 
-    private double[][] value;
+    private final double[][] value;
 
-    public Matrix(String valueStr) {
+    public Matrix(double[][] value) {
+        this.value = value.clone();
+    }
 
+    public Matrix(Matrix matrix) {
+        this.value = matrix.value.clone();
+    }
+
+    public Matrix(String strMatrix) {
+
+        String[] rowMatrix = strMatrix.split("},");
+        String[][] readyStrMatrix = new String[rowMatrix.length][];
+
+        for (int i = 0; i < rowMatrix.length; i++) {
+            readyStrMatrix[i] = rowMatrix[i].replaceAll("[{}]", "").split(",");
+        }
+
+        value = new double[readyStrMatrix.length][readyStrMatrix[0].length];
+
+        for (int i = 0; i < readyStrMatrix.length; i++) {
+            for (int j = 0; j < readyStrMatrix[i].length; j++) {
+                value[i][j] = Double.parseDouble(readyStrMatrix[i][j].trim());
+            }
+        }
+    }
+
+    public double[][] getValue() {
+        return value.clone();
+    }
+
+    @Override
+    public Var add(Var other) {
+        double[][] localValue = new double[value.length][value[0].length];
+        for (int i = 0; i < localValue.length; i++) {
+            System.arraycopy(value[i], 0, localValue[i], 0, localValue[i].length);
+        }
+        if (other instanceof Scalar scalar) {
+            for (int i = 0; i < localValue.length; i++) {
+                for (int j = 0; j < localValue[i].length; j++) {
+                    localValue[i][j] += scalar.getValue();
+                }
+            }
+            return new Matrix(localValue);
+        } else if (other instanceof Vector){
+            return super.add(other);
+        } else if (other instanceof Matrix matrix){
+            if (localValue.length == matrix.value.length && localValue[0].length == matrix.value[0].length){
+                for (int i = 0; i < localValue.length; i++) {
+                    for (int j = 0; j < localValue[i].length; j++) {
+                        localValue[i][j] += matrix.value[i][j];
+                    }
+                }
+                return new Matrix(localValue);
+            }
+        }
+        return super.add(other);
+    }
+
+    @Override
+    public Var sub(Var other) {
+        double[][] localValue = new double[value.length][value[0].length];
+        for (int i = 0; i < localValue.length; i++) {
+            System.arraycopy(value[i], 0, localValue[i], 0, localValue[i].length);
+        }
+
+        if (other instanceof Scalar scalar){
+            for (int i = 0; i < localValue.length; i++) {
+                for (int j = 0; j < localValue[i].length; j++) {
+                    localValue[i][j] -= scalar.getValue();
+                }
+            }
+            return new Matrix(localValue);
+        } else if (other instanceof Vector){
+            return super.sub(other);
+        } else if (other instanceof Matrix matrix) {
+            if (localValue.length == matrix.value.length && localValue[0].length == matrix.value[0].length) {
+                for (int i = 0; i < localValue.length; i++) {
+                    for (int j = 0; j < localValue[i].length; j++) {
+                        localValue[i][j] -= matrix.value[i][j];
+                    }
+                }
+                return new Matrix(localValue);
+            }
+            return super.sub(other);
+        }
+
+        return super.sub(other);
+    }
+
+    @Override
+    public Var mul(Var other) {
+            double[][] localValue = new double[value.length][value[0].length];
+            for (int i = 0; i < localValue.length; i++) {
+                System.arraycopy(value[i], 0, localValue[i], 0, localValue[i].length);
+            }
+
+            if (other instanceof Scalar scalar){
+                for (int i = 0; i < localValue.length; i++) {
+                    for (int j = 0; j < localValue[i].length; j++) {
+                        localValue[i][j] *= scalar.getValue();
+                    }
+                }
+                return new Matrix(localValue);
+            } else if (other instanceof Vector vector){
+                if (localValue[0].length == vector.getValue().length){
+                    double[] matrixMulVector = new double[localValue[0].length];
+                    for (int i = 0; i < localValue.length; i++) {
+                        for (int j = 0; j < localValue[i].length; j++) {
+                            matrixMulVector[i] += localValue[i][j] * vector.getValue()[j];
+                        }
+                    }
+                    return new Vector(matrixMulVector);
+                }
+                return super.mul(other);
+
+            } else if (other instanceof Matrix matrix){
+                if (localValue.length == matrix.value[0].length){
+                    double[][] mulMatrix = new double[localValue.length][matrix.value[0].length];
+                    for (int i = 0; i < localValue.length; i++) {
+                        for (int j = 0; j < matrix.value[0].length; j++) {
+                            for (int k = 0; k < matrix.value.length; k++) {
+                                mulMatrix[i][j] += localValue[i][k] * matrix.value[k][j];
+                            }
+                        }
+                    }
+                    return new Matrix(mulMatrix);
+                }
+                return super.mul(other);
+            }
+        return super.mul(other);
+    }
+
+    @Override
+    public Var div(Var other) {
+        return super.div(other);
     }
 
     @Override
     public String toString() {
-        return "Matrix{]";
+
+        StringBuilder out = new StringBuilder();
+        out.append('{');
+
+        for (int i = 0; i < value.length; i++) {
+            out.append('{');
+            String delimiter = "";
+            for (int j = 0; j < value[i].length; j++) {
+                out.append(delimiter).append(value[i][j]);
+                delimiter = ", ";
+                if (j == value[i].length - 1 && i < value.length - 1) {
+                    out.append('}').append(delimiter);
+                }
+            }
+            if (i == value.length - 1) {
+                out.append('}');
+            }
+        }
+        out.append('}');
+
+        return out.toString();
+
     }
 }
+
