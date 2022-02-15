4b40943d8d566f0e52a835ed56d5bc2fcdb4e6df
Maksim Skarabahaty <skaregok@gmail.com>
2022-02-15 19:20:06 +0300

    Skorobogatyi JD01_12 C3

--- src/by/it/skorobogatyi/jd01_12/TaskC2.java
+++ src/by/it/skorobogatyi/jd01_12/TaskC2.java
@@ -6,13 +6,26 @@ public class TaskC2 {
 
     public static void main(String[] args) {
 
+        Integer[] first = {1,2,3,4,4,4,5,5,5,6,6};
+        Integer[] second = {1,2,3,4,4,4,5,5,5,6,6};
+        Integer[] third = {4,4,5,5,9,9,8,7,6,5,4,6};
 
+        List<Integer> listLeft = Arrays.asList(first);
+        List<Integer> listRight = Arrays.asList(second);
+        List<Integer> Middle = Arrays.asList(third);
+
+        HashSet<Integer> leftSet = new HashSet<>(listLeft);
+        TreeSet<Integer> rightSet = new TreeSet<>(listRight);
+
+        Set<Integer> union = getUnion(leftSet, rightSet);
+        System.out.println(union);
+
+        Set<Integer> cross = getCross(leftSet, rightSet);
+        System.out.println(cross);
     }
 
     private static Set<Integer> getUnion(Set<?> ... sets) {
-        for (int i = 0; i < sets.length; i++) {
 
-        }
 
         return null;
     }
--- src/by/it/skorobogatyi/jd01_12/TaskC3.java
+++ src/by/it/skorobogatyi/jd01_12/TaskC3.java
@@ -5,8 +5,8 @@ import java.util.*;
 public class TaskC3 {
 
 
-    public static final String FOR_REMOVAL = "[^\\(\\)\\{\\}\\[\\]]+";
-    public static final String CHARS = "{[(";
+    public static final String CHARS_FOR_REMOVAL = "[^\\(\\)\\{\\}\\[\\]]+";
+    public static final String OPENING_BRACKETS = "[{(" ;
 
 
     public static void main(String[] args) {
@@ -23,24 +23,29 @@ public class TaskC3 {
 
     private static boolean process(String string) {
 
-        string = string.replaceAll(FOR_REMOVAL, "");
+        string = string.replaceAll(CHARS_FOR_REMOVAL, "");
 
         char[] elements = string.toCharArray();
 
         Deque<Character> charactersQueue = new ArrayDeque<>();
 
         for (char element : elements) {
-            if (CHARS.indexOf(element) >= 0) {
+            if (OPENING_BRACKETS.indexOf(element) >= 0) {
                 charactersQueue.addFirst(element);
+
             } else {
                 if (charactersQueue.isEmpty()) {
                     return false;
+
                 } else if (element == ']' && charactersQueue.peekFirst().equals('[')) {
                     charactersQueue.poll();
+
                 } else if (element == ')' && charactersQueue.peekFirst().equals('(')) {
                     charactersQueue.poll();
+
                 } else if (element == '}' && charactersQueue.peekFirst().equals('{')) {
                     charactersQueue.poll();
+
                 } else {
                     return false;
                 }
