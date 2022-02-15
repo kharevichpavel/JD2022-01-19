bff8526f41155162a2818a86d6bc39a554ec107f
Maksim Skarabahaty <skaregok@gmail.com>
2022-02-15 18:59:33 +0300

    Skorobogatyi JD01_12 C3

--- src/by/it/skorobogatyi/jd01_12/TaskC2.java
+++ src/by/it/skorobogatyi/jd01_12/TaskC2.java
@@ -1,4 +1,24 @@
 package by.it.skorobogatyi.jd01_12;
 
+import java.util.*;
+
 public class TaskC2 {
+
+    public static void main(String[] args) {
+
+
+    }
+
+    private static Set<Integer> getUnion(Set<?> ... sets) {
+        for (int i = 0; i < sets.length; i++) {
+
+        }
+
+        return null;
+    }
+
+    private static Set<Integer> getCross(Set<?> ... sets) {
+
+        return null;
+    }
 }
--- src/by/it/skorobogatyi/jd01_12/TaskC3.java
+++ src/by/it/skorobogatyi/jd01_12/TaskC3.java
@@ -1,4 +1,53 @@
 package by.it.skorobogatyi.jd01_12;
 
+import java.util.*;
+
 public class TaskC3 {
+
+
+    public static final String FOR_REMOVAL = "[^\\(\\)\\{\\}\\[\\]]+";
+    public static final String CHARS = "{[(";
+
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        String string = scanner.next();
+
+        boolean isStringCorrect = process(string);
+
+        System.out.println(isStringCorrect);
+
+
+    }
+
+    private static boolean process(String string) {
+
+        string = string.replaceAll(FOR_REMOVAL, "");
+
+        char[] elements = string.toCharArray();
+
+        Deque<Character> charactersQueue = new ArrayDeque<>();
+
+        for (char element : elements) {
+            if (CHARS.indexOf(element) >= 0) {
+                charactersQueue.addFirst(element);
+            } else {
+                if (charactersQueue.isEmpty()) {
+                    return false;
+                } else if (element == ']' && charactersQueue.peekFirst().equals('[')) {
+                    charactersQueue.poll();
+                } else if (element == ')' && charactersQueue.peekFirst().equals('(')) {
+                    charactersQueue.poll();
+                } else if (element == '}' && charactersQueue.peekFirst().equals('{')) {
+                    charactersQueue.poll();
+                } else {
+                    return false;
+                }
+            }
+
+        }
+        return charactersQueue.isEmpty();
+    }
 }
+
