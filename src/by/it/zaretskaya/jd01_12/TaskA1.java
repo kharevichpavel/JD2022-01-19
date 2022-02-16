8c3159b604f1643cea00c8a542af4faa565afbd1
galinaedyardovna <galinaedyardovnazaretskaya@gmail.com>
2022-02-16 08:06:46 +0300

    Task 1...12

---
+++ src/by/it/zaretskaya/jd01_12/TaskA1.java
@@ -0,0 +1,29 @@
+package by.it.zaretskaya.jd01_12;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+public class TaskA1 {
+    public static final int  MAX_GRADE = 10;
+    private static final int MAX_COUNT = 30;
+    private List<Integer> grades=new ArrayList<>();
+
+    public static void main(String[] args) {
+        Random random=new Random();
+        TaskA1 instance = new TaskA1();
+        int n=MAX_COUNT/2+ random.nextInt(15);
+        for (int i = 0; i < n; i++) {
+           int grade= random.nextInt(1+ MAX_GRADE);
+           instance.grades.add (grade);
+           instance.clearBad(instance.grades);
+
+            
+        }
+    }
+
+    private void clearBad(List<Integer> grades) {
+    }
+
+  
+}
