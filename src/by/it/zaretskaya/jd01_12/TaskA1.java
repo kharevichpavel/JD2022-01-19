330028e7f916894a04b173b7b092d1a4c29a907a
galinaedyardovna <galinaedyardovnazaretskaya@gmail.com>
2022-02-17 11:34:20 +0300

    Task 1...12

--- src/by/it/zaretskaya/jd01_12/TaskA1.java
+++ src/by/it/zaretskaya/jd01_12/TaskA1.java
@@ -1,6 +1,7 @@
 package by.it.zaretskaya.jd01_12;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 import java.util.Random;
 
@@ -14,16 +15,29 @@ public class TaskA1 {
         TaskA1 instance = new TaskA1();
         int n=MAX_COUNT/2+ random.nextInt(15);
         for (int i = 0; i < n; i++) {
-           int grade= random.nextInt(1+ MAX_GRADE);
-           instance.grades.add (grade);
-           instance.clearBad(instance.grades);
-
-            
+            int grade = random.nextInt(1 + MAX_GRADE);
+            instance.grades.add(grade);
         }
-    }
+        System.out.println(instance.grades);
+        instance.clearBad(instance.grades);
+        System.out.println(instance.grades);
+        }
 
+    @SuppressWarnings("Java8CollectionRemoveIf")
     private void clearBad(List<Integer> grades) {
+        Iterator<Integer> iterator= grades.iterator();
+        //noinspection Java8CollectionRemoveIf
+        while(iterator.hasNext()){
+            Integer grade = iterator.next();
+            if(grade<4){
+                iterator.remove();
+
+
+        }
+
+        }
+
     }
 
-  
+
 }
