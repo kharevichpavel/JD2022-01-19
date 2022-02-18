1e02869c6c94f6e06ef1002c2c116c669ec4378c
Chaikova Anna <ekaterina-pirogova@ro.ru>
2022-02-18 14:47:32 +0300

    Task 12

---
+++ src/by/it/chaikova/jd01_12/TaskA3.java
@@ -0,0 +1,28 @@
+package by.it.chaikova.jd01_12;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class TaskA3 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        ArrayList<Integer> integers = new ArrayList<>();
+        int positionZero = 0;
+        while (true) {
+            String word = scanner.next();
+            if (!word.equals("end")) {
+                int value = Integer.parseInt(word);
+                if (value == 0) {
+                    integers.add(positionZero, value);
+                } else if (value > 0) {
+                    integers.add(positionZero++, value);
+                } else {
+                    integers.add(value);
+                }
+            } else {
+                break;
+            }
+        }
+        System.out.println(integers);
+    }
+}
