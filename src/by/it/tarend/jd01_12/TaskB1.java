bafca49cc3ae83faee2ea03cd45a98c6a951c42d
MaximTarend <dedmasim@gmail.com>
2022-02-19 19:27:16 +0300

    Task 1..11 Tarend

---
+++ src/by/it/tarend/jd01_12/TaskB1.java
@@ -0,0 +1,23 @@
+package by.it.tarend.jd01_12;
+
+import java.util.HashMap;
+import java.util.Objects;
+import java.util.Scanner;
+import java.util.regex.Pattern;
+
+public class TaskB1 {
+
+    public static void main(String[] args) {
+
+        HashMap<String, Integer> hashMap = new HashMap<>();
+        Scanner sc = new Scanner(System.in);
+
+        while (true) {
+            String text = sc.nextLine();
+            if (!text.equals("end")) {
+                Pattern pattern = Pattern.compile("");
+            } else
+                break;;
+        }
+    }
+}
