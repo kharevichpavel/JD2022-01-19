99d0eaca856084687cdeba7478fa782e5f46a8b8
Palikk <7146514@gmail.com>
2022-02-19 16:03:14 +0300

    Task-12 B-3 done

---
+++ src/by/it/yushkevich/jd01_12/TaskC1.java
@@ -0,0 +1,33 @@
+package by.it.yushkevich.jd01_12;
+
+import java.util.HashMap;
+import java.util.Scanner;
+
+public class TaskC1 {
+
+    static HashMap<Integer, String> map = new HashMap<>();
+
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        int counter = 0;
+        //Filling map
+        while (scanner.hasNext()) {
+            String text = scanner.next();
+            if (!text.equals("end")) {
+                map.put(counter,text);
+                counter++;
+            } else {
+                break;
+            }
+        }
+        System.out.println(map.toString());
+
+        map.
+
+
+
+
+    }
+}
