30a218a8c29dc8a41bb63f8ee2ab3a35d5ed77a2
Palikk <7146514@gmail.com>
2022-02-20 23:55:15 +0300

    Task-14B - done

--- src/by/it/yushkevich/jd01_14/TaskB.java
+++ src/by/it/yushkevich/jd01_14/TaskB.java
@@ -13,54 +13,58 @@ public class TaskB {
     public static final String WORDS_REGEX = "[а-яА-ЯЁё]+";
     public static final String DOTS_REGEX = "[,.!?:;-]+";
     public static final String TXT = "resultTaskB.txt";
+    private static int dotsCounter = 0;
+    private static int wordCounter = 0;
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) {
+
 
         String fileName = PathFinder.getFileName(TaskA.class, ROOT, POEM_TXT);
-        List<String> list = new ArrayList<>();
+        countWordsAndDots(fileName);
 
-        BufferedReader reader = new BufferedReader(new FileReader(fileName));
-        String text;
-        int dotsCounter = 0;
-        int wordCounter = 0;
+        String writeTxtName = PathFinder.getFileName(TaskA.class, ROOT, TXT);
+        writeToTxt(dotsCounter, wordCounter, writeTxtName);
 
-        while ((text = reader.readLine()) != null) {
+    }
 
+    private static void countWordsAndDots(String fileName) {
+        try (BufferedReader reader = new BufferedReader(new FileReader(fileName))) {
 
-            Pattern patternWord = Pattern.compile(WORDS_REGEX);
-            Matcher matcherWord = patternWord.matcher(text);
+            String text;
 
-            Pattern patternDots = Pattern.compile(DOTS_REGEX);
-            Matcher matcherDots = patternDots.matcher(text);
+            while ((text = reader.readLine()) != null) {
 
-            while (matcherDots.find()) {
+                Pattern patternWord = Pattern.compile(WORDS_REGEX);
+                Matcher matcherWord = patternWord.matcher(text);
 
-                dotsCounter++;
+                Pattern patternDots = Pattern.compile(DOTS_REGEX);
+                Matcher matcherDots = patternDots.matcher(text);
 
+                while (matcherDots.find()) {
+                    dotsCounter++;
+                }
+                while (matcherWord.find()) {
+                    wordCounter++;
+                }
             }
-            while (matcherWord.find()) {
 
-                wordCounter++;
+            System.out.printf("words=%d, punctuation marks=%d", wordCounter, dotsCounter);
 
-            }
-
+        } catch (IOException e) {
+            throw new RuntimeException("Что-то пошло не так в попытке чтения файла", e);
         }
-        reader.close();
-
-        System.out.printf("words=%d, punctuation marks=%d", wordCounter,dotsCounter);
+    }
 
 
 
+    private static void writeToTxt(int dotsCounter, int wordCounter, String writeTxtName) {
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(writeTxtName))) {
 
-        String fileName2 = PathFinder.getFileName(TaskA.class, ROOT, TXT);
-        try (BufferedWriter writer = new BufferedWriter(new FileWriter(fileName2))) {
-
-            writer.write("words="+wordCounter+", punctuation marks="+dotsCounter);
+            writer.write("words=" + wordCounter + ", punctuation marks=" + dotsCounter);
 
 
-        } catch (IOException e){
+        } catch (IOException e) {
             throw new RuntimeException("OMG", e);
         }
-
     }
 }
