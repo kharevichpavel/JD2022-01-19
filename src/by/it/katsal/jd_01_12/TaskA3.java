e008cab55fe7622e1ed8d3c89ac1608237ac7d62
Andrey Katsal <katsal95@mail.ru>
2022-02-23 00:55:07 +0300

    jd2022-01-19 primerr

---
+++ src/by/it/katsal/jd_01_12/TaskA3.java
@@ -0,0 +1,32 @@
+package by.it.katsal.jd_01_12;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class TaskA3 {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        ArrayList<Integer> integers = new ArrayList<>();
+        int positionZero=0;
+        for(;;){
+            String word = sc.next();
+            if (!word.equals("end")) {
+                int value = Integer.parseInt(word);
+                if(value==0){
+                    integers.add(positionZero,value );
+                } else if (value>0){
+                    integers.add(positionZero++, value  );
+
+                }
+                else {
+                    integers.add(value);
+                }
+
+            } else {
+                break;
+            }
+
+        }
+        System.out.println(integers );
+    }
+}
