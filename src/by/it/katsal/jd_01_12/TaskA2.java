184227327ec386d6fa8f88072f39b5c1d9280212
Andrey Katsal <katsal95@mail.ru>
2022-02-23 00:28:12 +0300

    jd2022-01-19 primerr

--- src/by/it/katsal/jd_01_12/TaskA2.java
+++ src/by/it/katsal/jd_01_12/TaskA2.java
@@ -4,21 +4,21 @@ import java.util.*;
 
 public class TaskA2 {
     public static void main(String[] args) {
-        Integer[] left = {1,2,3,4,4,4,5,5,5,6,6};
+        Integer[] left = {1, 2, 3, 4, 4, 4, 5, 5, 5, 6, 6};
         List<Integer> listLeft = Arrays.asList(left);
         List<Integer> listRight = Arrays.asList(4, 4, 5, 5, 9, 9, 8, 7, 6, 5, 4, 6);
-        Set <Integer> leftSet = new HashSet<>(listLeft);
-        Set <Integer> rightSet = new TreeSet<>(listRight ) ;
+        Set<Integer> leftSet = new HashSet<>(listLeft);
+        Set<Integer> rightSet = new TreeSet<>(listRight ) ;
         System.out.printf("Left set=%s%n", leftSet );
         System.out.printf("Right set=%s%n", rightSet );
 
-        Set <Integer> union = getUnion(leftSet,rightSet );
+        Set<Integer> union = getUnion(leftSet,rightSet );
         System.out.printf("union set=%s%n", union  );
         System.out.printf("Left set=%s%n", leftSet );
         System.out.printf("Right set=%s%n", rightSet );
 
 
-        Set <Integer> cross = getCrossUnion(leftSet,rightSet );
+        Set<Integer> cross = getCross(leftSet,rightSet );
         System.out.printf("cross set=%s%n", cross );
         System.out.printf("Left set=%s%n", leftSet );
         System.out.printf("Right set=%s%n", rightSet );
@@ -26,14 +26,14 @@ public class TaskA2 {
 
     private static Set<Integer> getUnion(Set<Integer> leftSet, Set<Integer> rightSet) {
         HashSet<Integer> result = new HashSet<>(leftSet);
-        leftSet.addAll(rightSet ) ;
-        return result  ;
+        result.addAll(rightSet);
+        return result;
     }
 
-    private static Set<Integer> getCrossUnion(Set<Integer> leftSet, Set<Integer> rightSet) {
+    private static Set<Integer> getCross(Set<Integer> leftSet, Set<Integer> rightSet) {
 
         HashSet<Integer> result = new HashSet<>(leftSet);
-        leftSet.retainAll(rightSet ) ;
-        return result  ;
+        result.retainAll(rightSet);
+        return result;
     }
 }
