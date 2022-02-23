446c75311aa6b70efaee40a4c80951253080fefd
Alexander Khmelov <a.khmelev@gmail.com>
2022-02-23 18:29:17 +0300

    task 02-01

---
+++ src/by/it/_classwork_/jd02_01/entity/Customer.java
@@ -0,0 +1,15 @@
+package by.it._classwork_.jd02_01.entity;
+
+public class Customer {
+
+    private final String name;
+
+    public Customer(int number) {
+        name = "Customer №" + number;
+    }
+
+    @Override
+    public String toString() {
+        return name;
+    }
+}
