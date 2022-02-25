b98f09d856df394d8632def8210a3c8c7d473546
Alexander Khmelov <a.khmelev@gmail.com>
2022-02-25 11:39:59 +0300

    task 02-02 started

---
+++ src/by/it/_classwork_/jd02_02/Starter.java
@@ -0,0 +1,10 @@
+package by.it._classwork_.jd02_02;
+
+import by.it._classwork_.jd02_02.services.Store;
+
+public class Starter {
+    public static void main(String[] args) {
+        Store store = new Store();
+        store.start();
+    }
+}
