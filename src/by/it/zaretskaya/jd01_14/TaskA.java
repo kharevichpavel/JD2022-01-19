1e8f302cf78ebd5b5aa49c6cef2ed83b06518bfc
galinaedyardovna <galinaedyardovnazaretskaya@gmail.com>
2022-02-25 11:25:31 +0300

    TASK1....14

--- src/by/it/zaretskaya/jd01_14/TaskA.java
+++ src/by/it/zaretskaya/jd01_14/TaskA.java
@@ -1,24 +1,33 @@
 package by.it.zaretskaya.jd01_14;
 
-import java.awt.*;
 import java.io.*;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Locale;
 import java.util.concurrent.ThreadLocalRandom;
 
 public class TaskA {
 
     public static final String FILENAME_BYNARY = "data TaskA.bin";
     public static final String ROOT = "src";
+    public static final String TXT = "resultTaskA.txt";
 
     public static void main(String[] args) {
-        String filename=PathFinder.getFileName(TaskA.class, ROOT, FILENAME_BYNARY);
+        String filename = PathFinder.getFileName(TaskA.class, ROOT, FILENAME_BYNARY);
+        writeBinaryFile(filename);
+        List<Integer> integers = readBinaryFile(filename);
+        outputCONSOLE(integers);
+        String filenameTxt = PathFinder.getFileName(TaskA.class, ROOT, TXT);
+        outputToTxt(integers, filenameTxt);
+    }
+
+    private static void writeBinaryFile(String filename) {
         try (
-        DataOutputStream dataOutputStream = new DataOutputStream(
-                new BufferedOutputStream(
-                        new FileOutputStream(filename)
+                DataOutputStream dataOutputStream = new DataOutputStream(
+                        new BufferedOutputStream(
+                                new FileOutputStream(filename)
+                        )
                 )
-        )
         ) {
             for (int i = 0; i < 20; i++) {
                 int value = ThreadLocalRandom.current().nextInt(2000);
@@ -26,18 +35,46 @@ public class TaskA {
             }
 
         } catch (IOException e) {
-          throw  new RuntimeException("IO error",e);
+            throw new RuntimeException("IO error", e);
         }
+    }
 
-        List<Integer> intValues=new ArrayList<>();
-        try(
+    private static List<Integer> readBinaryFile(String filename) {
+        List<Integer> intValues = new ArrayList<>();
+        try (
 
-        DataInputStream dataInputStream = new DataInputStream(new BufferedInputStream(new FileInputStream(filename)))
-        ){
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
+                DataInputStream dataInputStream = new DataInputStream(new BufferedInputStream(new FileInputStream(filename)))
+        ) {
+            while (dataInputStream.available() > 0) {
+                int value = dataInputStream.readInt();
+                intValues.add(value);
+            }
         } catch (IOException e) {
             e.printStackTrace();
+        }
+        return intValues;
+    }
+
+    private static double outputCONSOLE(List<Integer> intValues) {
+        double sum = 0;
+        for (Integer intValue : intValues) {
+            sum += intValue;
+            System.out.printf("%d", intValue);
+        }
+        System.out.printf(Locale.ENGLISH, "%navg=%f%n", sum / intValues.size());
+        return sum;
+    }
+
+    private static void outputToTxt(List<Integer> intValues, String filenameTxt) {
+        try (PrintWriter printWriter = new PrintWriter(filenameTxt)) {
+            double sum2 = 0;
+            for (Integer intValue : intValues) {
+                sum2 += intValue;
+                printWriter.printf("%d", intValue);
+            }
+            printWriter.printf(Locale.ENGLISH, "%navg=%f%n", sum2 / intValues.size());
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException("IO error", e);
         }
     }
 }
