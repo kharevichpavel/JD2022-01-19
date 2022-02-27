280defd5bb06021a8c47ff6709600b323f2d5283
oliviyaK <oliviaolegovna@gmail.com>
2022-02-27 15:57:27 +0300

    Task01_15, LevelA

--- src/by/it/kustova/jd01_15/TaskA.java
+++ src/by/it/kustova/jd01_15/TaskA.java
@@ -3,18 +3,41 @@ package by.it.kustova.jd01_15;
 import by.it.kustova.jd01_14.PathFinder;
 
 import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.io.PrintWriter;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.List;
 import java.util.concurrent.ThreadLocalRandom;
 
 public class TaskA {
+    public static final String MATRIX_TXT = "matrix.txt";
+    public static final String ROOT = "src";
+
     public static void main(String[] args) {
         int [][] matrix = createMatrix(4,6,-15,15);
-        String filename =PathFinder.getFilename(TaskA.class, "src", "matrix.txt");
-        save(matrix, filename);
-
+        String filename =PathFinder.getFilename(TaskA.class, ROOT, MATRIX_TXT);
+        save2DArrayToTxtFile(matrix, filename);
+        List<String> strings = getStringsFromTxtFile(filename);
+        printToConsole(strings);
     }
 
-    private static void save(int[][] matrix, String filename) {
+    private static void printToConsole(List<String> strings) {
+        for (String string : strings) {
+            System.out.println(string);
+        }
+    }
+
+    private static List<String> getStringsFromTxtFile(String filename) {
+        try {
+            return Files.readAllLines(Paths.get(filename));
+        } catch (IOException e) {
+            throw new RuntimeException("IO error: "+filename, e);
+        }
+    }
+
+
+    private static void save2DArrayToTxtFile(int[][] matrix, String filename) {
        try (PrintWriter printWriter = new PrintWriter(filename)){
            for (int[] row : matrix) {
                for (int element : row) {
@@ -24,7 +47,7 @@ public class TaskA {
            }
              
         } catch (FileNotFoundException e) {
-            e.printStackTrace();
+           throw new RuntimeException("IO error: "+filename, e);
         }
     }
 
