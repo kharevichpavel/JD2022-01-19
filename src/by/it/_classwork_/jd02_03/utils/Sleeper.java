c464e1d2ea8b6dda3ad31ff4f3be78c1688577a3
Alexander Khmelov <a.khmelev@gmail.com>
2022-02-28 12:29:57 +0300

    classwork task 02-03 level A

---
+++ src/by/it/_classwork_/jd02_03/utils/Sleeper.java
@@ -0,0 +1,17 @@
+package by.it._classwork_.jd02_03.utils;
+
+import by.it._classwork_.jd02_03.exceptions.StoreException;
+
+public class Sleeper {
+
+    private Sleeper() {
+    }
+
+    public static void sleep(int millis) {
+        try {
+            Thread.sleep(millis / Constants.K_SPEED);
+        } catch (InterruptedException e) {
+            throw new StoreException(e);
+        }
+    }
+}
