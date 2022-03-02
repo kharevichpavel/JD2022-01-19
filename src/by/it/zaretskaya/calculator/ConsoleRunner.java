70dbeaf58731098a9ac947b8ff93deeb267828e8
galinaedyardovna <galinaedyardovnazaretskaya@gmail.com>
2022-03-02 08:06:23 +0300

    TASK1....15

--- src/by/it/zaretskaya/calculator/ConsoleRunner.java
+++ src/by/it/zaretskaya/calculator/ConsoleRunner.java
@@ -1,6 +1,8 @@
 package by.it.zaretskaya.calculator;
 
 import by.it.zaretskaya.calculator.controllers.MainController;
+import by.it.zaretskaya.calculator.repositories.MapRepository;
+import by.it.zaretskaya.calculator.repositories.VarRepository;
 import by.it.zaretskaya.calculator.service.CalcService;
 import by.it.zaretskaya.calculator.view.ConsolePrinter;
 import by.it.zaretskaya.calculator.view.Printer;
@@ -8,8 +10,8 @@ import by.it.zaretskaya.calculator.view.Printer;
 public class ConsoleRunner {
     public static void main(String[] args) {
         Printer printer = new ConsolePrinter();
-        VarRepository varRepository=new VarRepository();
-        CalcService calcService = new CalcService(varRepository);
+        VarRepository repository =new MapRepository();
+        CalcService calcService = new CalcService(repository);
         MainController mainController = new MainController(calcService);
         Application application = new Application(printer, calcService,mainController );
         application.run();
