99c07749154553d79c9302fc4765abb023c48501
KuzmaEV <yaugen93@yandex.ru>
2022-03-03 21:28:50 +0300

    Module2_1

---
+++ src/by/it/kuzma/jd02_01/entity/ShoppingCart.java
@@ -0,0 +1,9 @@
+package by.it.kuzma.jd02_01.entity;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ShoppingCart {
+
+   public final List<Good> goodsInCart = new ArrayList<>();
+}
---
+++ src/by/it/kuzma/jd02_01/utils/Constants.java
@@ -0,0 +1,5 @@
+package by.it.kuzma.jd02_01.utils;
+
+public class Constants {
+    public static final int K_SPEED=100;
+}
