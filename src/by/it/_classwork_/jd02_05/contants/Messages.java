806ff7a4600d8d73095915f3fb7a155e2c39251a
Alexander Khmelov <a.khmelev@gmail.com>
2022-03-04 12:38:58 +0300

    classwork task 02-05

---
+++ src/by/it/_classwork_/jd02_05/contants/Messages.java
@@ -0,0 +1,6 @@
+package by.it._classwork_.jd02_05.contants;
+
+public interface Messages {
+    String WELCOME = "messages.welcome";
+    String QUESTION = "messages.question";
+}
---
+++ src/by/it/_classwork_/jd02_05/resources/convert.bat
@@ -0,0 +1,4 @@
+set path=E:\Java\jdk-8\bin
+native2ascii -encoding utf-8 language_ru_RU.txt language_ru_RU.properties
+native2ascii -encoding utf-8 language_be_BY.txt language_be_BY.properties
+native2ascii -encoding utf-8 language.txt language.properties
