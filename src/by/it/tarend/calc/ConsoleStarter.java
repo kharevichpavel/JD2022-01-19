85e99d810cd5d9fa845d8205fba5d7267bfc5e2c
MaximTarend <dedmasim@gmail.com>
2022-03-06 21:14:19 +0300

    Tarend module02 1..2 AB, 3 A?, 4 A B?

--- src/by/it/tarend/calc/ConsoleStarter.java
+++ src/by/it/tarend/calc/ConsoleStarter.java
@@ -12,8 +12,8 @@ import by.it.tarend.calc.view.Printer;
 public class ConsoleStarter {
     public static void main(String[] args) {
         Printer printer = new ConsolePrinter();
-//        String fileName = PathFinder.getFileName(Application.class, "src", "vars.txt");
-        VarRepository repository = new MapRepository();
+        String fileName = PathFinder.getFileName(Application.class, "src", "vars.txt");
+        VarRepository repository = new FileRepository(fileName);
         CalcService calcService = new CalcService(repository);
         MainController mainController = new MainController(calcService);
         Application application = new Application(printer, mainController);
