ee3cacd71fbfaa65833cd803c15222692cd1a0db
bva2310 <vitalib2310@gmail.com>
2022-03-06 15:22:13 +0300

    Task jd02_04,jd02_04  (Level A) Burov

---
+++ src/by/it/burov/jd02_05/resources/commands.bat
@@ -0,0 +1,4 @@
+set path="C:\Program Files\Java\jdk1.8.0_231\bin"
+native2ascii -encoding utf-8 language.txt language.properties
+native2ascii -encoding utf-8 language_ru_RU.txt language_ru_RU.properties
+native2ascii -encoding utf-8 language_be_BY.txt language_be_BY.properties
